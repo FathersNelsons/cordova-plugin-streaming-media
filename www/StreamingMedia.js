aa6ce365772466aa92931eb6da0b46bed68a3462
FathersNelsons <grayson.udstrand@gmail.com>
2017-06-14 23:23:04 -0500

    Changing the JS file to remove OO stuff

--- www/StreamingMedia.js
+++ www/StreamingMedia.js
@@ -1,39 +1,6 @@
-"use strict";
-function StreamingMedia() {
-}
+var exec = require('cordova/exec')
 
-StreamingMedia.prototype.playAudio = function (url, options) {
+exports.playVideo = function (url, options) {
 	options = options || {};
-	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playAudio", [url, options]);
+	exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playVideo", [url, options]);
 };
-
-StreamingMedia.prototype.pauseAudio = function (options) {
-    options = options || {};
-    cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "pauseAudio", [options]);
-};
-
-StreamingMedia.prototype.resumeAudio = function (options) {
-    options = options || {};
-    cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "resumeAudio", [options]);
-};
-
-StreamingMedia.prototype.stopAudio = function (options) {
-    options = options || {};
-    cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "stopAudio", [options]);
-};
-
-StreamingMedia.prototype.playVideo = function (url, options) {
-	options = options || {};
-	cordova.exec(options.successCallback || null, options.errorCallback || null, "StreamingMedia", "playVideo", [url, options]);
-};
-
-
-StreamingMedia.install = function () {
-	if (!window.plugins) {
-		window.plugins = {};
-	}
-	window.plugins.streamingMedia = new StreamingMedia();
-	return window.plugins.streamingMedia;
-};
-
-cordova.addConstructor(StreamingMedia.install);
